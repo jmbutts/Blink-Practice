163481f9dbce648aed12fc4e2155269631139fdf
John Butts <force_of_will7@hotmail.com>
2022-05-20 18:58:14 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function showModal(contentArray, index) {
+  document.getElementById("overlay").style.display = "block"
+      console.log('showing modal for contentArray: ', contentArray, " and index: ", index)
+}
